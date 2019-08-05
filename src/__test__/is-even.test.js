c48a4ae4eed59bb2c66a951cb6541a9f5938dd0a
Tracy Williams <selpilot@gmail.com>
2019-08-05 15:44:30 -0700

    added eslint to config

--- src/__test__/is-even.test.js
+++ src/__test__/is-even.test.js
@@ -1,9 +1,9 @@
 const isEven = (val) => {
   return !(val % 2);
-}
+};
 
-describe("test for even number", () => {
-  it("24 is an even number", () => {
+describe('test for even number', () => {
+  it('24 is an even number', () => {
     expect(isEven(24)).toBe(true);
   });
 });
